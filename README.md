14f987a2029d23448919752480b5fee1dc15fb79
Quang Dang <quangdangfit@gmail.com>
2020-10-20 19:44:47 +0700

    init project structure

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ in a human readable format.
 * Feel free to use libraries or roll your own code as you see fit.
 * Where the data exists, values from any related entities **MUST** be included in
 the results, i.e.
-    * Searching **organization MUST** return its **ticket subject** and **users name**
-    * Searching **users MUST** return his/her **assignee ticket subject** and **submitted ticket subject** and his/her organization name
+    * Searching **organization MUST** return its **ticket subject** and **users name**.
+    * Searching **users MUST** return his/her **assignee ticket subject** and **submitted ticket subject** and his/her **organization name**.
     * Searching **tickets MUST** return its **assignee name**, **submitter name**, and **organization name**.
 * The user should be able to search on any field, full value matching is fine
 (e.g. "mar" won't return "mary").
---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Schema struct {
+	Data struct {
+		Organization string `mapstructure:"organization"`
+		User         string `mapstructure:"user"`
+		Ticket       string `mapstructure:"ticket"`
+	} `mapstructure:"data"`
+}
+
+var Config Schema
+
+func init() {
+	config := viper.New()
+	config.SetConfigName("config")
+	config.AddConfigPath(".")          // Look for config in current directory
+	config.AddConfigPath("config/")    // Optionally look for config in the working directory.
+	config.AddConfigPath("../config/") // Look for config needed for tests.
+	config.AddConfigPath("../")        // Look for config needed for tests.
+
+	config.SetEnvKeyReplacer(strings.NewReplacer(".", "__"))
+	config.AutomaticEnv()
+
+	err := config.ReadInConfig() // Find and read the config file
+	if err != nil {              // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	err = config.Unmarshal(&Config)
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	// fmt.Printf("Current Config: %+v", Config)
+}
